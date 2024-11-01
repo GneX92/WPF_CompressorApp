3918e713593c71a403965e1437f70ee5d4a90144
julian <julian.bliemel@gmx.de>
2024-11-01 12:50:54 +0100

    Fertig Gibt noch ein kleines Problem mit einer exception bei der ich nicht weiß warum er die wirft also Ignorier ich die erstmal

--- WPF_Compressor/MainWindow.xaml.cs
+++ WPF_Compressor/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using Microsoft.Win32;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,6 +9,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Runtime.CompilerServices;
+using System.IO.Compression;
+using System.IO;
 
 namespace WPF_Compressor;
 /// <summary>
@@ -18,5 +22,95 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private void dragMe( object sender , MouseButtonEventArgs e )
+    {
+        try
+        {
+            DragMove();
+        }
+        catch ( Exception )
+        {
+        }
+    }
+
+    private void CloseApp( object sender , RoutedEventArgs e )
+    {
+        Application.Current.Shutdown();
+    }
+
+    private void OpenFile( object sender , RoutedEventArgs e )
+    {
+        OpenFileDialog openFileDialog = new OpenFileDialog();
+
+        try
+        {
+            if ( openFileDialog.ShowDialog() == true )
+                textboxOpenPath.Text = openFileDialog.FileName;
+        }
+        catch ( Exception ex )
+        {
+            MessageBox.Show( ex.Message , "Error: Invalid path" , MessageBoxButton.OK , MessageBoxImage.Error );
+        }
+    }
+
+    private void SaveLocation( object sender , RoutedEventArgs e )
+    {
+        OpenFolderDialog openFolderDialog = new OpenFolderDialog();
+
+        try
+        {
+            if ( openFolderDialog.ShowDialog() == true )
+                textboxSavePath.Text = openFolderDialog.FolderName;
+        }
+        catch ( Exception ex )
+        {
+            MessageBox.Show( ex.Message , "Error: Invalid path" , MessageBoxButton.OK , MessageBoxImage.Error );
+        }
+    }
+
+    private void CompressFile( object sender , RoutedEventArgs e )
+    {
+        bool success = false;
+        string filePath = textboxOpenPath.Text;
+        string savePath = textboxSavePath.Text;
+
+        if ( string.IsNullOrEmpty( filePath ) || string.IsNullOrEmpty( savePath ) )
+        {
+            MessageBox.Show( "Please select both a file path and a save location." , "Error: Missing information" , MessageBoxButton.OK , MessageBoxImage.Error );
+            return;
+        }
+
+        try
+        {
+            var newfilePath = System.IO.Path.ChangeExtension( filePath , ".zip" );
+            string [] s = newfilePath.Split( @"\" );
+            string compressedFileName = s [ s.Length - 1 ];
+
+            savePath += @"\" + compressedFileName;
+
+            if ( !Directory.Exists( savePath ) )
+                Directory.CreateDirectory( savePath );
+
+            using FileStream originalFileStream = File.Open( filePath , FileMode.Open );
+            using FileStream compressedFileStream = File.Create( savePath );
+            using GZipStream compressor = new( compressedFileStream , CompressionMode.Compress );
+            originalFileStream.CopyTo( compressor );
+
+            success = true;
+        }
+        catch ( Exception ex )
+        {
+            if ( ex is not UnauthorizedAccessException )
+                MessageBox.Show( ex.Message , "Error: Compressing file" , MessageBoxButton.OK , MessageBoxImage.Error );
+            else
+                success = true;
+        }
+        finally
+        {
+            if ( success )
+                MessageBox.Show( "File compressed successfully." , "Success" , MessageBoxButton.OK , MessageBoxImage.Information );
+        }
     }
 }
